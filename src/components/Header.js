473deed0319cd36efa71576f083a57c4f78018c8
dauphin-davlon <dauphindavlon@gmail.com>
2023-09-20 10:18:56 -0400

    PUSHED

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+
+function Header(){
+    return(
+      <nav className="nav-bar">
+      <p>Logo</p>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/">Contact</a></li>
+        </ul>
+    </nav>
+    )
+  }
+  export default Header;
